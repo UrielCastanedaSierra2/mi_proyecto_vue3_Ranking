11492e6540a73157e41e0cbc7b68bdb069217a94
Uriel Castañeda Sierra <urielcastanedasierra@gmail.com>
2026-02-15 10:35:00 -0500

    V5.5 Incorpora control de Imagen No disponible

--- src/utils/images.js
+++ src/utils/images.js
@@ -6,11 +6,11 @@ import { PATH_FOTOS } from './paths'
 import { API_BASE } from './paths'
 
 export function obtenerImagen(nombreArchivo) {
-  /*
+  
   if (!nombreArchivo) {
-    return `${PATH_FOTOS}/no-image.jpg`
+    return `${PATH_FOTOS}/no-imagen.jpg`
   }
-  */
+  
   return `${PATH_FOTOS}/${nombreArchivo}`
   //para este ejemplo retorna.... `/imagenes/productos/${nombreArchivo}`  
 }
@@ -28,12 +28,18 @@ function esUrlAbsoluta(str) {
 //      en e HTML asigne la referencia usando esta función
 //      <img  :src="urlFoto(variable_que_contiene_el_dato_de_la_foto) />
 export function urlFoto(foto) {
+  // primero verificamos si viene informado el nombre o url de la foto
+  if (!foto) {
+    // si no viene foto, Fallback opcional (se muestra imagen o nada)
+    foto `${API_BASE}${PATH_FOTOS}/no-imagen.jpg`
+    //return '';    
+  }
+
   // Si viene de API pública con URL completa, úsa esa misma  (ya tiene la url completa).
   if (foto && esUrlAbsoluta(foto)) return foto;
 
-  // Si es local (solo nombre de archivo), servir desde backend/public/imagenes/productos
-  if (foto) return `${API_BASE}${PATH_FOTOS}/${foto}`;
+  // Si NO, entonces es porque solo se está informando el nombre del archivo
+  // (la foto es local o propia) servir desde el path definido para el backend
+  return `${API_BASE}${PATH_FOTOS}/${foto}`;
 
-  // Fallback opcional (imagen genérica o nada)
-  return '';
 }
